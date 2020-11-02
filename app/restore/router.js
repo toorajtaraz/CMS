bb7f7a8d1af73dbd2bcb3953a8192f5582d5a726
toorajtaraz <tooraj.info@gmail.com>
2020-11-02 21:50:57 +0330

    adding router to restore

--- app/restore/router.js
+++ app/restore/router.js
@@ -1,1 +1,7 @@
-const restoreHashPass = config.get('DEBUG');
+const controllers = require('./controllers');
+const auth = require('../../core/middleware/userMiddleware');
+
+module.exports = (router) => {
+    router.post('/restore', auth, controllers.post);
+    router.get('/restore', auth, controllers.get.getAllRestores);
+};
