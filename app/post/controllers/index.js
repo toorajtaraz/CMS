02359e22f0889701fdd0361755e5e7a210db24f8
Afarin Zamanian <afzado@gmail.com>
2020-11-02 19:25:51 +0330

    create controller files

---
+++ app/post/controllers/index.js
@@ -0,0 +1,13 @@
+const create = require('./create');
+const fetch = require('./fetch');
+const all = require('./all');
+const update = require('./update');
+const remove = require('./remove');
+
+module.exports={
+    create,
+    fetch,
+    all,
+    remove,
+    update,
+}
