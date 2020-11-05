e1b4e07a5d4f2a3945f62bc64daf074eda302399
Afarin Zamanian <afzado@gmail.com>
2020-11-05 16:23:12 +0330

    integrate user role into User

--- app/user/models/models.js
+++ app/user/models/models.js
@@ -3,6 +3,7 @@ const {mongoose} = require('../../../core/db/mongoose');
 const UserSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true,},
+    role: {type: mongoose.Schema.Types.ObjectId, ref: 'Role'},
     is_deleted: {type: Boolean, default: false,},
 });
 
@@ -11,18 +12,12 @@ const RoleSchema = new mongoose.Schema({
     is_deleted: {type: Boolean, default: false,},
 });
 
-const UserRoleSchema = new mongoose.Schema({
-    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', require: true,},
-    role: {type: mongoose.Schema.Types.ObjectId, ref: 'Role', require: true,},
-    is_deleted: {type: Boolean, default: false,},
-});
 
 const User = mongoose.model('User', UserSchema);
 const Role = mongoose.model('Role', RoleSchema);
-const UserRole = mongoose.model('UserRole', UserRoleSchema);
+
 
 module.exports = {
-    UserRole,
     User,
     Role,
 };
