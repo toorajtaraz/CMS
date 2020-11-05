5d30c5a76919d4f3cce722fa6938648a3d68a386
Afarin Zamanian <afzado@gmail.com>
2020-11-06 01:15:40 +0330

    update tag schema

--- app/post/models/model.js
+++ app/post/models/model.js
@@ -3,19 +3,19 @@ const {mongoose} =  require('../../../core/db/mongoose');
 const PostSchema = new mongoose.Schema({
     title: {type: String, required: true},
     content: {type: String, required: true},
-    summary: {type: String},
+    summary: {type: String, required: false},
     published: {type: Boolean, required: true, default: true},
     datePosted: {type: String, required: true},
-    dateUpdated: {type: String},
+    dateUpdated: {type: String, required: false},
     author: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
     editedBy: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
-    tags: [{type: mongoose.Schema.Types.ObjectId, ref: 'Tag'}],
+    tags: [{type: String, ref: 'Tag'}],
     is_deleted: {type: Boolean, default: false},
 })
 
 const TagSchema = new mongoose.Schema({
     // title: {type: String, required: true},
-    _id: String,
+    _id: {type: String, unique: true},
     dateCreated: {type: String, required: true},
     dateModified: {type: String},
     is_deleted: {type: Boolean, default: false},
