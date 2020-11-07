88030693e9758860eb3674ec994a29a01fad1847
Afarin Zamanian <afzado@gmail.com>
2020-11-07 22:10:01 +0330

    add update methods

---
+++ app/post/controllers/update.js
@@ -0,0 +1,104 @@
+const {ok, error} = require('../../../core/tools/response');
+const ajv = require('ajv')({allErrors: true});
+const service = require('../services/update');
+const validationSchema = require('../schemas/update');
+
+
+function validateData(data) {
+    const valid =  ajv.validate(validationSchema, data);
+    if (!valid)
+        return {valid: false, error: ajv.errors};
+    return {valid: true}
+}
+
+/**
+ * @api {put} /api/posts update
+ * @apiName update
+ * @apiGroup put
+ * @apiVersion 1.0
+ * 
+ * @apiParam {String} [title] Post title
+ * @apiParam {String} [content] Post content in markdown
+ * @apiParam {String} [summary] Post summary
+ * @apiParam {Boolean} [published] Publish Status
+ * @apiParam {String[]} [tags] Post tags by id 
+ * 
+ * @apiParamExample Request-Example:
+ *{
+ *     "content": "Something radical is going on.",
+ *     "summary": "shall we change?",
+ *     "title": "A New Era",
+ *     "tags": "tech"
+ * }
+ * @apiSuccess (200) {Object} result Updated post information
+ * @apiSuccessExample Success-Response:
+ {
+    "status": "ok",
+    "message": {
+        "en": "successfully updated.",
+        "fa": "پست به‌روز شد."
+    },
+    "result": {
+        "published": true,
+        "editedBy": [
+            "5fa44925d9d2fd1c84d6d18a"
+        ],
+        "tags": [
+            "tag1",
+            "tag2",
+            "tech"
+        ],
+        "is_deleted": false,
+        "_id": "5fa514e3bfa0c645fc457884",
+        "title": "A New Era",
+        "content": "Something radical is going on.",
+        "summary": "shall we change?",
+        "datePosted": "2020-11-08",
+        "author": "5fa44925d9d2fd1c84d6d18a",
+        "__v": 0,
+        "dateUpdated": "2020-11-07"
+    }
+}
+ * 
+ * @apiError (403) {Object} Forbidden Access is denied
+ * @apiErrorExample Forbidden
+ * HTTP/1.1 403 Forbidden
+ * {
+ *     "status": "error",
+ *     "message": {
+ *         "en": "action is blocked.",
+ *         "fa": "اجازه این عمل وجود ندارد."
+ *     }
+ * }
+ * @apiError (400) {Object} BadRequest Invalid data
+ * 
+ */
+
+const update = async (request, response, next) => {
+    const data = request.body;
+    const id = request.params.id;
+    const user = request.header('user');
+    // check user access (block status)
+    const hasAccess = await service.checkAccess(user);
+    if (!hasAccess){
+        return error(response, 403, {
+            en: 'action is blocked.',
+            fa: 'اجازه این عمل وجود ندارد.'
+        });
+    }
+    // validate data
+    const result = validateData(data);
+    if (!result.valid)
+    return error(response, 400, {
+        en: result.error
+    });
+    // debug(data);
+    const post = await service.update(id, data, user);
+    return ok(response, post, {
+        en: 'successfully updated.',
+        fa: 'پست به‌روز شد.',
+    });
+
+}
+
+module.exports = update;
