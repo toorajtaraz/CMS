7b4f85a302e5959a53e8b02cdf9f38690f72ede7
HamedGhoochanian <hamedghoochanian@gmail.com>
2020-11-13 17:22:02 +0100

    done

--- app/ads/models/schemas/type.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('../../../../core/db/mongoose');
-const {Schema} = mongoose;
-
-const typeSchema = new Schema({
-    name: {type:String,required: true},
-    cost: {type: Number, required: true}
-});
-
-module.exports = typeSchema
--- app/ads/router.js
+++ app/ads/router.js
@@ -2,7 +2,6 @@ const typesController = require('./types/controller');
 const couponController = require('./coupons/controllers');
 const adController = require('./controllers');
 
-
 module.exports = (app, router) => {
     app.get('/ads/types', typesController.get);
     app.get('/ads/coupons/id/:id', couponController.getById);
@@ -10,5 +9,5 @@ module.exports = (app, router) => {
     app.post('/ads', adController.create);
     app.get('/ads',adController.get);
     app.get('/ads/pay/:id', adController.requestPayment);
-    app.get('')
+    app.get('/ads/verifyPayment/:id',adController.verifyPayment);
 };
