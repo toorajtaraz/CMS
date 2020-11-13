06e23f5e750d765709a29cb3b914e4d4c649b096
Afarin Zamanian <afzado@gmail.com>
2020-11-13 13:52:21 +0330

    change default published status

--- app/post/models/model.js
+++ app/post/models/model.js
@@ -4,14 +4,14 @@ const PostSchema = new mongoose.Schema({
     title: {type: String, required: true},
     content: {type: String, required: true},
     summary: {type: String, required: false},
-    published: {type: Boolean, required: true, default: true},
+    published: {type: Boolean, default: false},
     datePosted: {type: String, required: true},
     dateUpdated: {type: String, required: false},
     author: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
     editedBy: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
     tags: [{type: String, ref: 'Tag'}],
     is_deleted: {type: Boolean, default: false},
-})
+});
 
 const TagSchema = new mongoose.Schema({
     // title: {type: String, required: true},
@@ -19,7 +19,7 @@ const TagSchema = new mongoose.Schema({
     dateCreated: {type: String, required: true},
     dateModified: {type: String},
     is_deleted: {type: Boolean, default: false},
-})
+});
 
 
 const Post = mongoose.model('Post', PostSchema, 'Post');
@@ -28,4 +28,4 @@ const Tag = mongoose.model('Tag', TagSchema, 'Tag');
 module.exports = {
     Post,
     Tag,
-}
+};
