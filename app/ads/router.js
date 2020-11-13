a09ff450b6cf37b303ccfec32e97b5a4e34d832d
HamedGhoochanian <hamedghoochanian@gmail.com>
2020-11-13 17:43:59 +0100

    changed app to router

--- app/ads/router.js
+++ app/ads/router.js
@@ -2,12 +2,12 @@ const typesController = require('./types/controller');
 const couponController = require('./coupons/controllers');
 const adController = require('./controllers');
 
-module.exports = (app, router) => {
-    app.get('/ads/types', typesController.get);
-    app.get('/ads/coupons/id/:id', couponController.getById);
-    app.get('/ads/coupons/code/:code', couponController.getByCode);
-    app.post('/ads', adController.create);
-    app.get('/ads',adController.get);
-    app.get('/ads/pay/:id', adController.requestPayment);
-    app.get('/ads/verifyPayment/:id',adController.verifyPayment);
+module.exports = (router) => {
+    router.get('/ads/types', typesController.get);
+    router.get('/ads/coupons/id/:id', couponController.getById);
+    router.get('/ads/coupons/code/:code', couponController.getByCode);
+    router.post('/ads', adController.create);
+    router.get('/ads',adController.get);
+    router.get('/ads/pay/:id', adController.requestPayment);
+    router.get('/ads/verifyPayment/:id',adController.verifyPayment);
 };
