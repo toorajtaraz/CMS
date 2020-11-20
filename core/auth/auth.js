648fb6eec8727a6cae59fb7d730236523937d066
tooraj taraz <tooraj.info@gmail.com>
2020-11-20 14:01:25 +0330

    Revert "auth.js edited online with Bitbucket"
    This reverts commit eeaeef64606070a3933a64907760602ac9a03d46.

--- core/auth/auth.js
+++ core/auth/auth.js
@@ -41,41 +41,11 @@ async function hashPass(pass) {
     const hash = await bcrypt.hash(pass, saltRounds);
     return hash;
 }
-async function isNumeric(str) {
-    if (typeof str != "string") return false  
-    return !isNaN(str) ;
-  }
-  async function period_is_in_range_hours(hours) {
-    if (!isNumeric(hours)) return false  ;
-    else if (Number(hours)<24 && Number(hours)>=0) return true;
-    else return false;
-  }
-  async function period_is_in_range_days(days) {
-    if (!isNumeric(days)) return false  ;
-    else if (Number(days)<31 && Number(days)>=0) return true;
-    else return false;
-  }
 
-  async function period_time_is_not_null(hours,days) {
-    if (!isNumeric(hours) && !isNumeric(days)) return false;
-    else if (!( (Number(hours)==0 && Number(days)==0) || (Number(hours)==null && Number(days)==null) )) return true;
-    else return false;
-  }
-
-  async function period_is_acceptabel (hours , days){
-    return (await period_is_in_range_hours(hours) &&
-            await period_is_in_range_days(days) &&
-            await period_time_is_not_null(hours , days)
-    );
-};
 module.exports = {
     isPassValid,
     hashPass,
     signKey,
     signData,
     extractData,
-    period_is_in_range_hours,
-    period_is_in_range_days,
-    period_time_is_not_null,
-    period_is_acceptabel
 }
