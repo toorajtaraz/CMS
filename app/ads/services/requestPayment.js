f2a7da26d190e712154a87f436851dd72006f0f9
HamedGhoochanian <hamedghoochanian@gmail.com>
2020-11-23 15:44:05 +0100

    fixed initiate payment

--- app/ads/services/requestPayment.js
+++ app/ads/services/requestPayment.js
@@ -1,9 +1,9 @@
 const models = require('../models/model');
 
 
-const getAd = async (id) => await models.Ad.find(id);
+const getAd = async (id) => await models.Ad.findById(id);
 const giveAdaReceipt = async (adId, authority, amount) => {
-    const ad = await models.Ad.find(id);
+    const ad = await models.Ad.findById(adId);
     const receipt = await (new models.Receipt({amount, authority})).save();
     return models.Ad.findOneAndUpdate({_id: ad._id}, {receipt: receipt._id});
 };
